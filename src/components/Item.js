33b3702c9c530df17ca0a832a83e3d7117c5bc71
ka-few <njorovista@gmail.com>
2025-07-14 07:11:29 +0300

    Add state and events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsIncart] = useState(false)
+
+  function handleClick() {
+    setIsIncart(!isInCart)
+  }
+
+  const liClassList = ["", "in-cart"]
+  const buttonTextList = ["Add to Cart", "Remove From Cart"]
   return (
-    <li className="">
+    <li className={liClassList[Number(isInCart)]}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
+      <button onClick={handleClick}>{buttonTextList[Number(isInCart)]}</button>
+    </li> 
   );
 }
 
